04fc53830e06273b01ea5d494c55e2acd8b175c8
David Suarez <gcvanguardia@gmail.com>
2024-04-22 06:21:48 -0500

    feat: protect routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -3,12 +3,13 @@ import { Redirect, Route } from 'react-router-dom';
 import { IonRouterOutlet } from '@ionic/react';
 import Home from './pages/home/Home';
 import Login from "./pages/login/Login";
+import ProtectedRoute from './shared/components/auth/protected-route/ProtectedRoute'; // Importa ProtectedRoute desde el nuevo archivo
 
 const Routes: React.FC = () => (
   <IonRouterOutlet>
     <Route exact path="/"> <Redirect to="/login" /> </Route>
     <Route exact path="/login"> <Login /> </Route>
-    <Route exact path="/home"> <Home /> </Route>
+    <ProtectedRoute exact path="/home" component={Home} />
   </IonRouterOutlet>
 );
 
